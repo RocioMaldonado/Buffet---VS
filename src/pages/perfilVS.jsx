8b2ae2a3f33cbdf3b9aecc436716b1c90b043656
Rocio Maldonado <138414271+RocioMaldonado@users.noreply.github.com>
2025-11-06 18:32:24 -0300

    Create perfilVS.jsx

---
+++ src/pages/perfilVS.jsx
@@ -0,0 +1,55 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import "../styles/Perfil.css";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+function Perfil() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Navbar />
+      <div className="perfil-container">
+        {/* Encabezado con flecha */}
+          <div className="perfil-header">
+            <button className="perfil-back" onClick={() => navigate(-1)}>
+            <AiOutlineArrowLeft size={20} />
+          </button>
+            <h2 className="perfil-titulo">Mi Cuenta</h2>
+          </div>
+          <div className="perfil-card">
+            <h3 className="perfil-subtitulo">Editar perfil</h3>
+
+          <div className="perfil-bienvenida">
+            <h3>¡Hola!</h3>
+            <p>Desde aquí podés editar tu información personal y mantener tu cuenta actualizada.</p>
+          </div>
+
+          <div className="perfil-info">
+            <div className="perfil-input-card">
+              <label>Nombre</label>
+              <input type="text" placeholder="Tu nombre" />
+            </div>
+
+            <div className="perfil-input-card">
+              <label>Correo electrónico</label>
+              <input type="email" placeholder="correo@ejemplo.com" />
+            </div>
+
+            <div className="perfil-input-card">
+              <label>Contraseña</label>
+              <input type="password" placeholder="********" />
+            </div>
+          </div>
+
+          <div className="perfil-actions">
+            <button className="perfil-boton guardar">Guardar</button>
+            <button className="perfil-boton cancelar">Cancelar</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Perfil;
