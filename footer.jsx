c25d67f8233de3f927c24f2ad1a6fb92df8417ed
Rocio Maldonado <138414271+RocioMaldonado@users.noreply.github.com>
2025-11-08 02:32:40 -0300

    Create footer.jsx

---
+++ footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+       <div class="footer-left">
+    <p>Universidad Nacional Guillermo Brown</p>
+    <p>Blas Parera 132, Burzaco</p>
+  </div>
+  <div class="footer-right">
+    <p>© 2025 Buffet UNaB. All rights reserved.</p>
+  </div>
+</footer>
+  );
+};
+
+export default Footer;
